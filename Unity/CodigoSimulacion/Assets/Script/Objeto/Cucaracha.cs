f9449190890f0044dfe7753e9f03079c8e57b8ed
handala88 <64783266+handala88@users.noreply.github.com>
2020-05-24 20:28:39 +0200

    Unity
    Ejecutable y proyecto entero de Unity
    
    Co-Authored-By: berea1999 <berea1999@users.noreply.github.com>

---
+++ Unity/CodigoSimulacion/Assets/Script/Objeto/Cucaracha.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Cucaracha : MonoBehaviour
+{
+
+    public bool viva = true;
+
+    
+    void Update()
+    {
+        transform.position = new Vector3(-65.1f, 2.1f, -28.6f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cuerpo" && viva)
+        {
+            transform.rotation = Quaternion.EulerAngles(90f, 0, 0);
+            viva = false;
+        }
+    }
+}
---
+++ Unity/CodigoSimulacion/Assets/Script/Objeto/Cucaracha1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Cucaracha1 : MonoBehaviour
+{
+
+    public bool viva = true;
+
+    void Update()
+    {
+        transform.position = new Vector3(-19f, 2.1f, -55.5f);
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cuerpo" && viva)
+        {
+            transform.rotation = Quaternion.EulerAngles(90f, 0, 0);
+            viva = false;
+        }
+    }
+}
